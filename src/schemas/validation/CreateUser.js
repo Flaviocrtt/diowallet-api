0ab317b915768b78d478e4167a9f6bd061f050e9
Flavio Ceratti <flavio777crtt@hotmail.com>
2025-04-02 01:14:29 -0300

    fix: validação de email para criar usuário

--- src/schemas/validation/CreateUser.js
+++ src/schemas/validation/CreateUser.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const CreateUser = Joi.object({
     name: Joi.string().required().min(3),
-    emai: Joi.object(),
+    email: Joi.string(),
     password: Joi.string().required().min(3),
     createdAt: Joi.string(),
 });
